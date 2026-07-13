55df89f91c03c2457b850025d8b4ce5c99dae162
hyunah0119 <shinhsim@gmail.com>
2026-07-13 17:14:54 +0900

    ios 키보드 입력 이슈

--- todo-mood-app/src/layouts/MainLayout.tsx
+++ todo-mood-app/src/layouts/MainLayout.tsx
@@ -2,14 +2,34 @@ import Header from "@/components/common/Header"
 import Footer from "@/components/common/Footer"
 import { Outlet } from 'react-router-dom'
 import { useThemeStore } from '@/store/themeStore'
+import { useEffect } from "react"
 
 const MainLayout = () => {
   const { isDarkMode } = useThemeStore();
 
+  useEffect(() => {
+    const setAppHeight = () => {
+      const viewportHeight = window.visualViewport?.height ?? window.innerHeight;
+      document.documentElement.style.setProperty("--app-height", `${viewportHeight}px`);
+    }
+
+    setAppHeight();
+
+    window.addEventListener("resize", setAppHeight);
+    window.visualViewport?.addEventListener("resize", setAppHeight);
+    window.visualViewport?.addEventListener("scroll", setAppHeight);
+
+    return () => {
+      window.removeEventListener("resize", setAppHeight);
+      window.visualViewport?.removeEventListener("resize", setAppHeight);
+      window.visualViewport?.removeEventListener("scroll", setAppHeight);
+    }
+  }, []);
+
   return (
     <div className={`${isDarkMode ? 'dark' : ''} font-pretendard font-light text-[#333]`}>
-      <div className="min-h-screen bg-neutral-100 dark:bg-neutral-950 flex justify-center dark:text-white">
-        <div className="w-full max-w-107.5 h-dvh min-h-0 flex flex-col bg-white dark:bg-neutral-900 overflow-hidden relative">
+      <div className="min-h-(--app-height) bg-neutral-100 dark:bg-neutral-950 flex justify-center dark:text-white">
+        <div className="w-full max-w-107.5 h-(--app-height) min-h-0 flex flex-col bg-white dark:bg-neutral-900 overflow-hidden relative">
           <Header />
           <div className="flex flex-1 min-h-0 w-full flex-col overflow-hidden">
             <Outlet />
